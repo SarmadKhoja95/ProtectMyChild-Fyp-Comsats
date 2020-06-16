cf93fcb7c10efe2872ae159c004eb13c038d4516
sarmad.khoja <sarmadkhoja.sk@gmail.com>
2020-06-16 11:45:31 +0500

    Report selection UI added

--- screens/SelectReports.js
+++ screens/SelectReports.js
@@ -6,34 +6,45 @@ import {
   
 } from "react-native";
 import { Block, Text, Button, Icon } from 'galio-framework';
+import pic from "../assets/report.png"
 
 import Constants from "expo-constants";
 
-export default function SelectReports({ navigation }) {
+export default function SelectReports( props ) {
   
 
   return (
     <Block style={styles.container}>
-      <Block flex={0.3} style={{ width:"100%",justifyContent:"flex-start",alignItems:"center",paddingTop:15}}>
-        <Block height={150} width={150} style={{ backgroundColor: "#bdbfbf", borderWidth: 0.5 }}>
+      <Block flex={0.2} style={{ width:"100%",justifyContent:"flex-start",alignItems:"center",paddingTop:15}}>
+        <Block middle height={150} width={150} >
           <Image
-            style={{ height: 150, width: 150 }}
-            source={{
-              uri: "https://randomuser.me/api/portraits/men/1.jpg"
-            }}
+            style={{ height: 100, width: 100 }}
+            source={pic}
           />
           <Block middle>
-              <Text h4>Reports</Text>
+              <Text h5 bold color="grey">Complaints</Text>
           </Block>
         </Block>
       </Block>
-      <Block flex={0.6} style={{ width: "85%", padding:15, backgroundColor:"", borderWidth:0.5, borderColor:"grey"}}>
-        <Block row space="between" style={{ height:"50%", width:"100%",backgroundColor:"lightblue" }}>
+      <Block flex={0.5} style={{ width: "85%"}}>
+        <Block space="between" style={{ height:"35%", width:"100%",padding:15,backgroundColor:"#74BBFB" }}>
+          <Text size={45} color="white" >0</Text>
+          <Block><Text size={22} color="white">Total complaints</Text></Block>
+        </Block>
+        <Block row space="between" style={{ height:"35%", marginTop:"3%", width:"100%" }}>
+          <Block style={{backgroundColor:"#e43a39",width:"48%",padding:15}}>
+          <Text size={35} color="white" >0</Text>
+          <Block><Text size={20} color="white">Open Complaints</Text></Block>
+          </Block>
+          <Block style={{backgroundColor:"#50C878",width:"48%",padding:15}}>
+          <Text size={35} color="white" >0</Text>
+          <Block><Text size={20} color="white">Closed Complaints</Text></Block>
+          </Block>
         </Block>
       </Block>
-      {/* <Block flex={0.15} middle>
-      <Button size="small" color="maroon" shadowless style={{ borderWidth: 1, marginTop: 2 }}><Text size={18} color="white">Remove</Text></Button>
-      </Block> */}
+      <Block flex={0.25} bottom style={{ width:"100%"}}>
+      <Button onPress={()=>props.navigation.navigate("AddReport")} onlyIcon icon="plus" iconFamily="Feather" iconSize={30} iconColor="white" size="small" color="maroon" style={{ position:"absolute",bottom:0,right:25, width:60,height:60 }} />
+      </Block>
     </Block >
   );
 }
