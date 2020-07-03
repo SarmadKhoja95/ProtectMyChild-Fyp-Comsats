46f134b0f29da02954c21b49f889a1eee660029e
sarmad.khoja <sarmadkhoja.sk@gmail.com>
2020-07-04 01:36:38 +0500

    Analytics data integerated with db

--- screens/Analytics.js
+++ screens/Analytics.js
@@ -10,68 +10,19 @@ import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import MapView, { Marker, Circle } from 'react-native-maps';
 
+//redux state
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function Analytics(props) {
 
   const [status, setStatus] = useState("pending");
   const [location, setLocation] = useState({ longitude: null, latitude: null });
   const [radius, setRadius] = useState(1000);
+  const [isZone, setZone] = useState(false);
   const [area, setArea] = useState({ longitude: 2, latitude: 2 });
   const areaHandler = useRef();
   const circle = createRef(null);
-
-  const areas = [{
-    title: '8:05 PM',
-    type:"report",
-    coordinates: {
-        latitude: 33.481195,
-        longitude: 73.0853384,
-    },
-  },
-  {
-    title: '9:06 PM',
-    type:"lost",
-    coordinates: {
-        latitude: 33.489608,
-        longitude: 73.0850208,
-    },
- 
-  },
-  {
-    title: 'hey',
-    type:"report",
-    coordinates: {
-        latitude: 32.489618,
-        longitude: 73.1853384,
-    },
-
-  },,
-  {
-    title: 'hey',
-    type:"report",
-    coordinates: {
-        latitude: 32.489618,
-        longitude: 72.1853384,
-    },
-
-  },
-  {
-    title: 'hey',
-    type:"lost",
-    coordinates: {
-        latitude: 31.489618,
-        longitude: 73.1853384,
-    },
-
-  },
-  {
-    title: 'hey',
-    type:"lost",
-    coordinates: {
-        latitude: 33.489618,
-        longitude: 73.1853384,
-    },
-  }]
+  const report = useSelector(state => state.report.data.data.result);
 
   useEffect(() => {
     areaHandler.current = area;
@@ -100,6 +51,9 @@ export default function Analytics(props) {
         <>
             <MapView
               zoomEnabled
+              showsUserLocation
+              showsMyLocationButton
+              moveOnMarkerPress
               initialRegion={{
                 latitude: location.coords.latitude,
                 longitude: location.coords.longitude,
@@ -107,41 +61,31 @@ export default function Analytics(props) {
                 longitudeDelta: 0.0421,
               }} style={styles.mapStyle}>
                 <>
-                    { areas.map((area) => {
-                        return <Circle
-                              center={area.coordinates}
-                              radius={radius}
-                              fillColor={ area.type == "report" ? "rgba(178,34,34 , 0.6)" : "rgba(112, 181, 44, 0.52)" }
-                              strokeColor="rgba(85, 85, 100, 0.52)"
-                              />        })
+                    { report.map((row) => {
+                        return ( <>
+                                <Marker
+                                coordinate={row.location}
+                              />
+                              <Circle
+                              center={row.location}
+                              radius={row.radius}
+                              fillColor="rgba(178,34,34 , 0.4)"
+                              strokeColor="rgba(85, 85, 100, 0.42)"
+                              />
+                              </> );})
                      }
                 </>
             </MapView>
-            {/* <Block style={{ position: "absolute", top: 50, left: 10 }}>
-              <Block>
-                <TouchableOpacity onPress={() => setRadius(radius + 200)}>
-                  <Icon color="#95a5a6" name="plussquare" family="AntDesign" size={30} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setRadius(radius - 200)}>
-                  <Icon color="#95a5a6" style={{ marginTop: 20 }} name="minussquare" family='AntDesign' size={30} />
-                </TouchableOpacity>
-              </Block>
-            </Block> */}
             <Block style={{ position: "absolute", top: 50, right: 20 , backgroundColor:"white",padding:10}}>
               <Block row style={{display:"flex",alignItems:"center"}}> 
                 <Icon color="rgb(178,34,34)" name="controller-record" family="Entypo" size={30} />
                 <Text size={15} style={{paddingLeft:5}}>Reported cases</Text>
               </Block>
-              <Block row style={{display:"flex",alignItems:"center"}}>
-                <Icon color="rgb(112, 181, 44)" name="controller-record" family="Entypo" size={30} />
-                <Text size={15} style={{paddingLeft:5}}>Lost cases</Text>
-              </Block>
             </Block>
-            <Block style={{ position: "absolute", bottom: 20 , backgroundColor:"maroon",padding:15,borderRadius:30}}>
-              <Block row style={{display:"flex",alignItems:"center"}}> 
-                
-                <Text size={18} color="white" style={{paddingLeft:5}}>You are in safe zone</Text>
-              </Block>
+              <Block style={{ position: "absolute", bottom: 20 , backgroundColor:"green",padding:15,borderRadius:30}}>
+               <Block row style={{display:"flex",alignItems:"center"}}> 
+               <Text size={18} color="white" style={{paddingLeft:5}}>You are in safe zone</Text>
+             </Block>
             </Block>
         </>
         :
