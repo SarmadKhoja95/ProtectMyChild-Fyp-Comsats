39a98dd5c778124889f9d1eab1452958e067f78e
wajahatkhan4321 <heliumakali12@gmail.com>
2020-07-06 00:59:12 +0500

    Nearby reports api added

--- api/report/reportAction.js
+++ api/report/reportAction.js
@@ -5,8 +5,17 @@ import {
   GET_USER_REPORTS_SUCCESS,
   ADD_USER_REPORTS_REQUEST,
   ADD_USER_REPORTS_SUCCESS,
+  GET_NEARBY_REPORTS_REQUEST,
+  GET_NEARBY_REPORTS_SUCCESS,
+  GET_ZONE_REPORTS_REQUEST,
+  GET_ZONE_REPORTS_SUCCESS
 }
   from '../types';
+
+const isEmpty = require("lodash/isEmpty");
+
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 import { v4 as uuidv4 } from "react-native-uuid";
 import Firebase from "../../firebase";
@@ -58,7 +67,7 @@ export const getReports = (token) => async dispatch => {
   };
   try {
     let res =await axios.get(config.apiUrl + "reports/", options);
-    setProfilePicture(res);
+    setProfilePicture(res.data.data.result);
     dispatch({
       type: GET_USER_REPORTS_SUCCESS,
       payload: { msg: "success", data: res.data },
@@ -71,12 +80,118 @@ export const getReports = (token) => async dispatch => {
     });
   }
 }
+    // get All nearby other user reports
 
-export async function setProfilePicture(res){
-        let result = res.data.data.result
+export const getNearbyReports = (token) => async dispatch => {
+
+  dispatch({ type: GET_NEARBY_REPORTS_REQUEST });
+  // Request Headers
+  const options = {
+    headers: {
+      'Content-type': config.ContentType,
+      'x-auth-token': token
+    },
+  };
+  try {
+    let res =await axios.get(config.apiUrl + "reports/all", options);
+    let allreports = res.data.data.result;
+    let location = await getLocation();
+    let nearby = allreports.filter((val)=>{
+      let check = false;
+      if(isEmpty(val.location) == false){
+        //console.log(val.name);
+        check = loc( location.latitude, location.longitude, val.location.latitude, val.location.longitude, 5 )
+      }
+      if(check){
+        console.log(val.name);
+        return val;
+      }
+    })
+    setProfilePicture(nearby);
+    dispatch({
+      type: GET_NEARBY_REPORTS_SUCCESS,
+      payload:  { result: nearby , total: nearby.length }
+    });
+  }
+  catch (e) {
+    dispatch({
+      type: GET_NEARBY_REPORTS_SUCCESS,
+      payload: false
+    });
+  }
+}
+
+export const checkIsZone = (token) => async dispatch => {
+
+  dispatch({ type: GET_ZONE_REPORTS_REQUEST });
+  // Request Headers
+  const options = {
+    headers: {
+      'Content-type': config.ContentType,
+      'x-auth-token': token
+    },
+  };
+  try {
+    let res =await axios.get(config.apiUrl + "reports/all", options);
+    console.log("in get ZONE reports");
+    let allreports = res.data.data.result;
+    let location = await getLocation();
+    let isZone = false;
+    let v = allreports.filter((val)=>{
+      let check = false;
+      if(isEmpty(val.location) == false){
+        check = loc( location.latitude, location.longitude, val.location.latitude, val.location.longitude, parseFloat(val.radius/1000) );
+      }
+      if(check){
+       return val;
+      }
+    })
+     if( isEmpty(v) === false ){
+       isZone = true;
+     }
+    dispatch({
+      type: GET_ZONE_REPORTS_SUCCESS,
+      payload:  { result: allreports, isZone},
+    });
+  }
+  catch (e) {
+    dispatch({
+      type: GET_ZONE_REPORTS_SUCCESS,
+      payload: false
+    });
+  }
+}
+
+export async function setProfilePicture(result){
          for (let i = 0; i < result.length; i++) {
          let ref = Firebase.storage().ref().child("report/" + result[i].profilePicture);
          let url = await ref.getDownloadURL();
          result[i].profilePicture = url;
         }
 }
+
+function loc(userLatitude, userLongitude, midpointLatitude, midpointLongitude, rad) {
+
+  let ky = 40000 / 360;
+
+  let kx = Math.cos(Math.PI * midpointLatitude / 180.0) * ky;
+  let dx = Math.abs(midpointLongitude - userLongitude) * kx;
+  let dy = Math.abs(midpointLatitude - userLatitude) * ky;
+  let final = Math.sqrt(dx * dx + dy * dy);
+  
+  if (final < rad)
+    return true;
+
+}
+
+const getLocation = async () => {
+  let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  if (status === "granted") {
+    let location = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    let result = { longitude: parseFloat(location.coords.longitude), latitude: parseFloat(location.coords.latitude) };
+    return result;
+  }
+  else {
+    getLocation();
+  }
+};
--- api/report/reportReducer.js
+++ api/report/reportReducer.js
@@ -1,14 +1,18 @@
 /* eslint-disable prettier/prettier */
 import {
   GET_USER_REPORTS_SUCCESS,
-  ADD_USER_REPORTS_SUCCESS
+  ADD_USER_REPORTS_SUCCESS,
+  GET_NEARBY_REPORTS_SUCCESS,
+  GET_ZONE_REPORTS_SUCCESS
 }
   from '../types';
 
 const initialState = {
   data: {},
   msg: "",
-  isAdd: false
+  isAdd: false,
+  isNearby: false,
+  isZone: false
 };
 
 export default function (state = initialState, action) {
@@ -17,12 +21,22 @@ export default function (state = initialState, action) {
       return {
         ...state,
         data: action.payload.data,
-        msg: action.payload.msg
+        msg: action.payload.msg,
       };
     case ADD_USER_REPORTS_SUCCESS:
       return {
         ...state,
         isAdd: action.payload
+      };
+      case GET_NEARBY_REPORTS_SUCCESS:
+      return {
+        ...state,
+        isNearby: action.payload
+      };
+      case GET_ZONE_REPORTS_SUCCESS:
+      return {
+        ...state,
+        isZone: action.payload
       };
     default:
       return state;
